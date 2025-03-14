64c5e71d85d556a67039d93c7beef8d5a68d9c04
Nazar Voinarovych <nvoinarovych@Nazars-Voinarovych-MacBook-Pro.local>
2025-03-14 15:16:23 +0200

    first result

--- force-app/main/default/lwc/cards/cards.js
+++ force-app/main/default/lwc/cards/cards.js
@@ -1,7 +1,12 @@
 
 import {LightningElement} from 'lwc';
 import image1 from '@salesforce/resourceUrl/image1';
+import image2 from '@salesforce/resourceUrl/image2';
+import image3 from '@salesforce/resourceUrl/image3';
+
 
 export default class Cards extends LightningElement {
     imageUrl=image1;
+    imageUrl2=image2;
+    imageUrl3=image3;
 }
---
+++ force-app/main/default/lwc/footer/footer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nvoinarovych on 14.03.2025.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class Footer extends LightningElement {
+
+}
