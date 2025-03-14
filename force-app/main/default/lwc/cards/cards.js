0d494dd5750e5b4fad613c90932a94aacfd57b87
Nazar Voinarovych <nvoinarovych@Nazars-Voinarovych-MacBook-Pro.local>
2025-03-14 11:58:09 +0200

    bad cards.html with huge svg code

--- force-app/main/default/lwc/cards/cards.js
+++ force-app/main/default/lwc/cards/cards.js
@@ -1,9 +1,7 @@
-/**
- * Created by nvoinarovych on 13.03.2025.
- */
 
 import {LightningElement} from 'lwc';
+import image1 from '@salesforce/resourceUrl/image1';
 
 export default class Cards extends LightningElement {
-
+    imageUrl=image1;
 }
